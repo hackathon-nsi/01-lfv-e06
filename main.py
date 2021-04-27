e981c172fddf3b492ea28e1dab3e9337e39dc43f
nsi-lfv-kap <79848851+nsi-lfv-kap@users.noreply.github.com>
2021-04-27 11:42:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -70,19 +70,49 @@ def jade():
 
 
 def eloi():
-    box1 = (400, 160, 440, 190)
-    region1 = im.crop(box1)
+    im = Image.open(urllib.request.urlopen('https://raw.githubusercontent.com/hackathon-nsi/h7n-nsi-01/main/images/washington.bmp'))
 
-    box2 = (300, 150, 340, 180)
-    region2 = im.crop(box2)
+    # On crée une liste de variables box
+    box_list = ["box1", "box2", "box3", "box4", "box5", "box6", "box7", "box8"]
 
-    box3 = (250, 160, 290, 190)
-    region3 = im.crop(box3)
+    # une liste de variables de régions
+    region_list = ["region1", "region2", "region3", "region4", "region5", "region6", "region7", "region8"]
 
-    box4 = (350, 200, 390, 230)
-    region4 = im.crop(box4)
+    # Création manuelle(provisoire) des valeurs de bords gauches
+    bords_gauches = [150, 200, 250, 300, 350, 400, 450, 500]
 
-    # Intervertion
+    # Création avec une boucle des bords droits qui sont les bords gauches + 40
+    bords_droits = []
+    for i in range(0,len(bords_gauches) -1):
+        bords_droits.append(bords_gauches[i] + 40)
+
+    # Création manuelle(provisoire) des valeurs de bords hauts
+    bords_hauts = [160, 150, 160, 150, 160, 150, 160, 150]
+
+    # Création avec une boucle des bords bas qui sont les bords hauts + 30
+    bords_bas = []
+    for i in range(0,len(bords_hauts) -1):
+        bords_bas.append(bords_hauts[i] + 30)
+
+    # Attribuer les morceaux d'images (boites) à des variables
+    # créer les listes de coordonnées
+    coord = []
+    for (g, d, h, b) in zip(bords_gauches,bords_droits,bords_hauts,bords_bas):
+        coord.append((g,d,h,b))
+
+
+    # Maintenant on attribue les coordonnées aux variables
+    for i in range(0,len(coord)):
+        globals()[box_list[i]] = coord[i]
+
+
+    # On crope les régions définies ci-dessus (box...)
+    for i in range(0,len(coord)):
+        globals()[region_list[i]] = im.crop(coord[i])
+
+
+    # et maintenant on réordonne les crops et on les affiches
+    # Test manuel (je ferai une boucle une fois validé) mais qui ne fonctionne pas .....
     im.paste(region4, box1)
 
     im.paste(region3, box2)
@@ -90,8 +120,10 @@ def eloi():
     im.paste(region1, box3)
 
     im.paste(region2, box4)
-
+    # Afficher l'image transformée
     im.show()
+
+eloi()
 
 def philip():
     r=0
